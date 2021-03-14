de041b104afdedde0b9706572ee6d0de9188c60f
AndriyZ4 <80629311+AndriyZ4@users.noreply.github.com>
2021-03-14 15:14:20 +0200

    Add files via upload

---
+++ js/copi.js
@@ -0,0 +1,19 @@
+let clipboard = new ClipboardJS('.btn');
+
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+});
+
+function toClipboard(el) {
+    el.setAttribute('data-clipboard-text', location.href + '');
+    let clipboard = new ClipboardJS(el);
+
+    clipboard.on('success', function(e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+    });
+}
